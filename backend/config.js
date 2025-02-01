fe4ab03e316d6f0f4eb03beef7e88f8d54ccbd2b
Sahil <118348625+Sahil-958@users.noreply.github.com>
2025-02-01 12:04:00 +0530

    changed the deafult host with localhost

--- backend/config.js
+++ backend/config.js
@@ -2,8 +2,8 @@
 export const config = {
   ALLOWED_EXTENSIONS: ["png", "jpg", "jpeg", "webp"],
   MAX_FILE_SIZE: 20 * 1024 * 1024, // 20MB
-  PORT: process.env.PORT || 5000,
-  HOST: process.env.HOST || "127.0.0.1",
+  PORT: process.env.PORT || 5100,
+  HOST: process.env.HOST || "localhost",
   MODELS: {
     CAPTION: "Xenova/vit-gpt2-image-captioning",
     OBJECT_DETECTION: "Xenova/detr-resnet-50",
