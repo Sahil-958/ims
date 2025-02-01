30828d0bfc2eaf78ab8abb3f003f4508db6ed0ca
Sahil <118348625+Sahil-958@users.noreply.github.com>
2025-02-01 12:04:27 +0530

    removed uncessaary routes

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,16 +2,13 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import CaptionPage from "./routes/CaptionPage.jsx";
 import { HashRouter, Routes, Route } from "react-router";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <HashRouter>
       <Routes>
-        <Route path="/" element={<App />}>
-          <Route path="caption" element={<CaptionPage />} />
-        </Route>
+        <Route path="/" element={<App />}></Route>
       </Routes>
     </HashRouter>
   </StrictMode>,
--- frontend/src/routes/CaptionPage.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-
-function CaptionPage() {
-  console.log("CaptionPage");
-  return <div className="bg-red-500 h-52 w-52">CaptionPage</div>;
-}
-
-export default CaptionPage;
